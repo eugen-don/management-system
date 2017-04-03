6449ee4a8b0906f734ef64253e5709d05173ace5
Eugen Don <eugendon87@gmail.com>
2017-04-03 14:20:36 +0200

    test

--- mgmtsystem_action/tests/test_create_action.py
+++ mgmtsystem_action/tests/test_create_action.py
@@ -116,7 +116,7 @@ class TestModelAction(common.TransactionCase):
             "type_action": "immediate",
         })
         stage_ids = self.env['mgmtsystem.action.stage'].search([])
-        stages_found = record._stage_groups({}, stages, domain, order, None)
+        stages_found = record._stage_groups({}, stage_ids, domain, order, None)
         state = (len(stage_ids) == len(stages_found[0]))
         self.assertTrue(state)
 
