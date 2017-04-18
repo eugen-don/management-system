c3a5da574c675c8bc197432d335fd303087f836c
Eugen Don <eugendon87@gmail.com>
2017-04-18 16:15:03 +0200

    asdas

--- mgmtsystem_action/tests/test_create_action.py
+++ mgmtsystem_action/tests/test_create_action.py
@@ -117,7 +117,7 @@ class TestModelAction(common.TransactionCase):
         })
         stage_ids = self.env['mgmtsystem.action.stage'].search_count([])
         stages_found = record._stage_groups(self, record, stage_ids)
-        state = (len(stage_ids) == stages_found[0])
+        state = (stage_ids == len(stages_found[0]))
         self.assertTrue(state)
 
     def test_send_mail(self):
